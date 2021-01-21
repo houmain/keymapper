7cbd0fa5b6ad45352f9537e2235fac4b2b251286
houmain <houmain@posteo.net>
2021-01-21 19:14:58 +0100

    Refactoring

--- src/linux/server/keyboard.cpp
+++ src/linux/server/keyboard.cpp
@@ -12,6 +12,71 @@
 const auto EVDEV_MINORS = 32;
 
 namespace {
+  bool is_keyboard(int fd) {
+    auto version = int{ };
+    if (::ioctl(fd, EVIOCGVERSION, &version) == -1 ||
+        version != EV_VERSION)
+      return false;
+
+    auto devinfo = input_id{ };
+    if (::ioctl(fd, EVIOCGID, &devinfo) != 0)
+      return false;
+
+    switch (devinfo.bustype) {
+      case BUS_USB:
+      case BUS_I8042:
+      case BUS_ADB:
+        break;
+      default:
+        return false;
+    }
+
+    const auto required_bits = (1 << EV_SYN) | (1 << EV_KEY) | (1 << EV_REP);
+    auto bits = 0;
+    if (::ioctl(fd, EVIOCGBIT(0, sizeof(bits)), &bits) == -1 ||
+        (bits & required_bits) != required_bits)
+      return false;
+
+    return true;
+  }
+
+  bool wait_until_keys_released(int fd) {
+    const auto retries = 1000;
+    const auto sleep_ms = 5;
+    for (auto i = 0; i < retries; ++i) {
+      auto bits = std::array<char, (KEY_MAX + 7) / 8>();
+      if (::ioctl(fd, EVIOCGKEY(bits.size()), bits.data()) == -1)
+        return false;
+
+      const auto all_keys_released =
+        std::none_of(std::cbegin(bits), std::cend(bits),
+          [](char bits) { return (bits != 0); });
+      if (all_keys_released)
+        return true;
+
+      ::usleep(sleep_ms * 1000);
+    }
+    return false;
+  }
+
+  bool grab_keyboard(int fd, bool grab) {
+    return (::ioctl(fd, EVIOCGRAB, (grab ? 1 : 0)) == 0);
+  }
+
+  int open_event_device(int index) {
+    const auto paths = { "/dev/input/event%d", "/dev/event%d" };
+    for (const auto path : paths) {
+      auto buffer = std::array<char, 128>();
+      std::snprintf(buffer.data(), buffer.size(), path, index);
+      do {
+        const auto fd = ::open(buffer.data(), O_RDONLY);
+        if (fd >= 0)
+          return fd;
+      } while (errno == EINTR);
+    }
+    return -1;
+  }
+
   bool read_all(int fd, char* buffer, size_t length) {
     while (length != 0) {
       auto ret = ::read(fd, buffer, length);
@@ -25,71 +90,6 @@ namespace {
     return true;
   }
 } // namespace
-
-bool is_keyboard(int fd) {
-  auto version = int{ };
-  if (::ioctl(fd, EVIOCGVERSION, &version) == -1 ||
-      version != EV_VERSION)
-    return false;
-
-  auto devinfo = input_id{ };
-  if (::ioctl(fd, EVIOCGID, &devinfo) != 0)
-    return false;
-
-  switch (devinfo.bustype) {
-    case BUS_USB:
-    case BUS_I8042:
-    case BUS_ADB:
-      break;
-    default:
-      return false;
-  }
-
-  const auto required_bits = (1 << EV_SYN) | (1 << EV_KEY) | (1 << EV_REP);
-  auto bits = 0;
-  if (::ioctl(fd, EVIOCGBIT(0, sizeof(bits)), &bits) == -1 ||
-      (bits & required_bits) != required_bits)
-    return false;
-
-  return true;
-}
-
-bool wait_until_keys_released(int fd) {
-  const auto retries = 1000;
-  const auto sleep_ms = 5;
-  for (auto i = 0; i < retries; ++i) {
-    auto bits = std::array<char, (KEY_MAX + 7) / 8>();
-    if (::ioctl(fd, EVIOCGKEY(bits.size()), bits.data()) == -1)
-      return false;
-
-    const auto all_keys_released =
-      std::none_of(std::cbegin(bits), std::cend(bits),
-        [](char bits) { return (bits != 0); });
-    if (all_keys_released)
-      return true;
-
-    ::usleep(sleep_ms * 1000);
-  }
-  return false;
-}
-
-bool grab_keyboard(int fd, bool grab) {
-  return (::ioctl(fd, EVIOCGRAB, (grab ? 1 : 0)) == 0);
-}
-
-int open_event_device(int index) {
-  const auto paths = { "/dev/input/event%d", "/dev/event%d" };
-  for (const auto path : paths) {
-    auto buffer = std::array<char, 128>();
-    std::snprintf(buffer.data(), buffer.size(), path, index);
-    do {
-      const auto fd = ::open(buffer.data(), O_RDONLY);
-      if (fd >= 0)
-        return fd;
-    } while (errno == EINTR);
-  }
-  return -1;
-}
 
 std::vector<int> grab_keyboards() {
   auto fds = std::vector<int>();
