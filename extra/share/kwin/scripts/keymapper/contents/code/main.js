1514a44b058a6b1b958d290b7b4be15f88431e9f
houmain <houmain@posteo.net>
2024-08-16 21:15:55 +0200

    Added KDE6 support to keymapper KWin script

--- extra/share/kwin/scripts/keymapper/contents/code/main.js
+++ extra/share/kwin/scripts/keymapper/contents/code/main.js
@@ -1,4 +1,14 @@
-workspace.clientActivated.connect(function(client){
+
+const isKDE6 = typeof workspace.windowList === 'function';
+
+let connectWindowActivated;
+if (isKDE6) {
+    connectWindowActivated = (handler) => workspace.windowActivated.connect(handler);
+} else {
+    connectWindowActivated = (handler) => workspace.clientActivated.connect(handler);
+}
+
+connectWindowActivated(function(client){
     callDBus(
         "com.github.houmain.Keymapper",
         "/com/github/houmain/Keymapper",
