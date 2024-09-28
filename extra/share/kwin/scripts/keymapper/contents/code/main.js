a3643aaf3c8ae796997f7bb4371c548835b79a87
Roderick Gibson <roderickgit@roderickgibson.net>
2024-09-28 01:42:13 -0700

    fix invalid index lookup on null objects

--- extra/share/kwin/scripts/keymapper/contents/code/main.js
+++ extra/share/kwin/scripts/keymapper/contents/code/main.js
@@ -14,7 +14,7 @@ connectWindowActivated(function(client){
         "/com/github/houmain/Keymapper",
         "com.github.houmain.Keymapper",
         "WindowFocus",
-        "caption" in client ? client.caption : "",
-        "resourceClass" in client ? client.resourceClass : ""
+        client?.caption ?? "",
+        client?.resourceClass ?? ""
     );
 });
